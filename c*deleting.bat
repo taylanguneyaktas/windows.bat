7ccb3ac7fc6f3b7672a519df0325f46f1eb47bdf
794 <info@taylanguneyaktas.com>
2022-04-22 22:54:13 +0300

    Create c*deleting.bat

---
+++ c*deleting.bat
@@ -0,0 +1,20 @@
+@echo off
+echo i'm hacking your computer stupid man!
+pause
+echo i don't like you!
+pause
+echo So i will delete all your files!
+pause
+echo c:/DELETING FILES
+pause
+echo c:/DELETING SYSTEM32 FILES
+pause
+echo c:/FILES HAVE BEEN DELETED
+pause
+echo your computer will shutdown in..
+msg * 5 
+msg * 4 
+msg * 3 
+msg * 2 
+msg * 1 sucker!!
+shutdown -s -t 30
