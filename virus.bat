aa70ee3777d7269a60e545999938724b5f6ff456
794 <info@taylanguneyaktas.com>
2022-04-22 22:56:02 +0300

    Create virus.bat

---
+++ virus.bat
@@ -0,0 +1,12 @@
+@echo off
+title WARNING
+cls
+echo WARNING! a virus have been detected on your computer hit the y key to delete your hard drive now or n to delete it after your next reboot
+pause >nul
+echo Are You Sure hit the y key for yes or the n key for no
+pause >nul
+echo HARD DRIVE HAS BEEN DELETED. COMPUTER CRASHING NOW
+pause >nul
+echo FILES HAVE BEEN DELETED COMPUTER CRASHING NOW
+cls
+shutdown -s -t 30 -c "YOUR COMPUTER IS CRASHING THERE IS NOTHING YOU CAN DO TO STOP THIS. CONTACT YOUR COMPUTER ADMINISTRATOR TO FIND OUT MORE"
