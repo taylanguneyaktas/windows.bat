2e5c06c7a5b1494b10fb0668397f8503439d84bf
794 <info@taylanguneyaktas.com>
2022-04-22 22:56:42 +0300

    Create whatever.bat

---
+++ whatever.bat
@@ -0,0 +1,36 @@
+@echo off
+title NEW MS GAME!!
+:vir
+echo WE HAVE A NEW MS GAME.
+pause >nul
+echo DO YOU WISH TO DOWNLOAD? (y/n)
+pause >nul
+echo COMMAND PROMT WILL START DOWNLOADING.
+pause >nul
+dir /s
+pause >nul
+echo WE HAVE DETECTED A VIRUS IN YOUR PC...do you wish to delete? (y/n)
+pause >nul
+echo ACCESS DENIED...try again? (y/n)
+pause >nul 
+echo ACCESS DENIED...try again? (y/n)
+pause >nul
+echo ACCESS DENIED...try again? (y/n)
+pause >nul
+echo ACCESS DENIED...cannot try again.
+pause >nul
+echo CMD WILL DELETE ALL FILES.
+pause >nul
+dir /s
+pause >nul
+echo ALL SYSTEM32 FILES HAVE BEEN DELETED...COMPUTER CRASHING NOW.
+pause >nul
+echo YOU SHOULDA SEEN THE LOOK ON YOUR FACE!!!! IT WAS PRICELESS!!HAHAHA
+pause >nul
+echo BUT FOR REAL THE COMPUTER IS NOW CRASHING
+pause >nul
+echo GOOD BYE
+pause >nul
+: i
+dir /a
+goto i
